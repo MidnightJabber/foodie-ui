87fd003954faf78f8fd737d9fa51b923632d2cf7
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2018-05-20 18:30:37 -0400

    Init Repo

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from 'components/Header';
+// import { Route, Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
