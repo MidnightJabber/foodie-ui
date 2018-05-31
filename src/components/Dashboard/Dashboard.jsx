18cfb7d297a033539f284b2f02831c2eb3267668
Vishrut Reddi <vishrutreddi@gmail.com>
2018-05-30 22:01:15 -0700

    Add Header Component (#26)
    resolves #19

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import Header from 'components/Header/Header';
+
 const Dashboard = () => (
-  <div className="dashboard" />
+  <div className="dashboard">
+    <Header />
+  </div>
 );
 
 export default Dashboard;
