e4b91dfac13af3d3e64ed3747bf1576630db99c5
Vishrut Reddi <vishrutreddi@gmail.com>
2019-02-23 01:13:37 -0800

    Add routing related to login/logout and worked on header component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from 'components/Header/Header';
+import './Dashboard.scss';
 
 // TODO: Move all the place components in their own area
 const Dashboard = () => (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,103 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { graphql } from 'react-apollo';
+import { faCogs } from '@fortawesome/fontawesome-free-solid';
+import SimpleButton from 'components/SimpleButton/SimpleButton';
+import { Redirect } from 'react-router-dom';
+import Routes from 'utils/routes';
+import currentUserQuery from 'queries/currentUser';
+import PropTypes from 'prop-types';
+import logoutMutation from 'mutations/logout';
 import './Header.scss';
 
-const Header = () => (
-  <div className="header">
-    <div className="header-user-circle" />
-  </div>
-);
+class Header extends Component {
+  constructor() {
+    super();
 
-export default Header;
+    this.state = {
+      userCircleOptionsShown: false,
+    };
+  }
+
+  onLogoutClick = () => {
+    const { logout } = this.props;
+    logout({
+      refetchQueries: [{ query: currentUserQuery }],
+    });
+  }
+
+  getDashboardRoute = () => (
+    <div className="header">
+      <div className="header-logo">FitMi</div>
+      <div
+        role="button"
+        tabIndex={0}
+        className="header-user-circle"
+        onKeyDown={this.userClircleOnClick}
+        onClick={this.userClircleOnClick}
+      />
+      { this.showUserCircleOptions() }
+    </div>
+  );
+
+  userClircleOnClick = () => {
+    this.setState({ userCircleOptionsShown: !this.state.userCircleOptionsShown });
+  }
+
+  showUserCircleOptions = () => {
+    if (this.state.userCircleOptionsShown) {
+      return (
+        <div className="user-circle-options-container">
+          <div className="options-header-container">
+            <div className="profile-image" />
+            <div className="options-header">
+              <div className="name">Vishrut Reddi</div>
+              <div className="email">vishrutreddi@gmail.com</div>
+            </div>
+          </div>
+          <ul className="options">
+            <li>
+              <FontAwesomeIcon className="icon" icon={faCogs} />
+              <div className="text">Settings</div>
+            </li>
+          </ul>
+          <div className="options-footer-container">
+            <SimpleButton title="Logout" onBtnClick={this.onLogoutClick} />
+          </div>
+        </div>
+      );
+    }
+
+    return null;
+  }
+
+  /**
+   * Based on if an active cookie for a logged in user exists, we direct to the
+   * dashboard or to the landing page
+   * @return {React.Component | Route} Component or Route(component) to render
+   */
+  render = () => {
+    const { loading, user } = this.props.data;
+
+    if (loading) {
+      return (<div>Loading...</div>);
+    } else if (!loading && user) {
+      return this.getDashboardRoute();
+    }
+
+    return (<Redirect to={Routes.Root} />);
+  }
+}
+
+Header.propTypes = {
+  logout: PropTypes.func,
+  data: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+};
+
+
+Header.defaultProps = {
+  logout: () => null,
+  data: {},
+};
+
+export default graphql(currentUserQuery)(graphql(logoutMutation, { name: 'logout' })(Header));
